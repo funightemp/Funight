2ef78921ade63407fd7da5c0327e6b00439925ff
GuiBorges-19 <borgesnicoguilherme@sapo.pt>
2025-03-20 15:33:26 +0000

    Atualizações no main e criação do requirements.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
-from gateway.routes import health
+from backend.gateway.routes import health
+
+#uvicorn backend.main:app --reload --port 8001   
+
 
 app = FastAPI(title="FastApi App")
 
