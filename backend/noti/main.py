4827fd1bc5e38c5abc669908df6a63bf5eab36c1
Eduardo Vasconcelos <eduardopmvasconcelos@gmail.com>
2025-03-26 15:15:53 +0000

    cloud messaging a funcionar

--- backend/noti/main.py
+++ backend/noti/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+import firebase_admin
+from firebase_admin import credentials, messaging
 
 app = FastAPI()
+
+# Inicializar o Firebase
+cred = credentials.Certificate("firebase_credentials.json")
+firebase_admin.initialize_app(cred)
 
 @app.get("/")
 def read_root():
     return {"message": "Microserviço de Notificações está funcionando!"}
+
+@app.post("/send-notification/")
+def send_notification(token: str, title: str, body: str):
+    message = messaging.Message(
+        notification=messaging.Notification(
+            title=title,
+            body=body,
+        ),
+        token=token,
+    )
+    response = messaging.send(message)
+    return {"message": "Notificação enviada!", "response": response}
