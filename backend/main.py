e5efda6afd361c3225601c92c4151712e0dbfa66
GuiBorges-19 <borgesnicoguilherme@sapo.pt>
2025-03-26 14:47:34 +0000

    alterações no port

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+import os
+import uvicorn
 from backend.gateway.routes import health
 
-#uvicorn backend.main:app --reload --port 8001   
+#uvicorn backend.main:app --reload --port 8000 
 
 
 app = FastAPI(title="FastApi App")
@@ -11,3 +13,7 @@ app.include_router(health.router)
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app,"0.0.0.0", port=port)
