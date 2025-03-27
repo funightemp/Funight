3fdb746641c45986f48b3b08319f2bb455b0694e
GuiBorges-19 <borgesnicoguilherme@sapo.pt>
2025-03-27 16:07:51 +0000

    criação do model

--- backend/gateway/models/enums.py
+++ backend/gateway/models/enums.py
@@ -1,8 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, ForeignKey, Numeric, Enum, TIMESTAMP, func
-from sqlalchemy.orm import relationship
-from database.connection import base
 import enum
-
 
 #Enums: tipos para uma dada categoria
 class tipeUser(str, enum.Enum):
---
+++ backend/gateway/models/user_model.py
@@ -0,0 +1,4 @@
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, Numeric, Enum, TIMESTAMP, func
+from sqlalchemy.orm import relationship
+from database.connection import base
+
